054f1c0ee854a72c96cf5338961d6c76b4d5ccfd
lexi <329002252@qq.com>
2019-12-22 12:26:50 +0800

    完成用户列表

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Router from 'vue-router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
-
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+return originalPush.call(this, location).catch(err => err)
+}
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   // console.log(config);
